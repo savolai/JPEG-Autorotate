604a8380cd3bd110723348d19cb3f35566f1e10a
Olli Savolainen <inbox@pilpi.net>
2013-05-21 12:19:51 +0300

    Rebranded bat files

--- autooperatedir_ft.bat
+++ autooperatedir_ft.bat
@@ -1,13 +1,13 @@
 @SET temppath=%PATH%
 @SET PATH=%PATH%;%3
-@echo JPEG-EXIF autorotate - http://pilpi.net/software/JPEG-EXIF_autorotate.php
+@echo JPEG Autorotate - http://pilpi.net/software/JPEG-EXIF_autorotate.php
 @echo Rotating images in the selected directory - please wait. On some systems startup may even take a couple of minutes. 
 @echo.
 %1 %4 -ft %2\*.jpg
 @SET PATH=%temppath%
 @echo.
 @echo ----------------------
-@echo JPEG-EXIF autorotate - http://pilpi.net/software/JPEG-EXIF_autorotate.php
+@echo JPEG Autorotate - http://pilpi.net/software/JPEG-EXIF_autorotate.php
 @echo Rotating finished. Please press any key to close the window.
 @echo.
 @pause
